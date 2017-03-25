0e1a90b6200293f206dc81f7cbdafd432aeec44a
JS Morisset <jsm@surniaulula.com>
2017-03-25 01:28:29 -0700

    v1.2.0-1

--- jsm-user-locale.php
+++ jsm-user-locale.php
@@ -255,10 +255,10 @@ if ( ! class_exists( 'JSM_User_Locale' ) ) {
 					$plugin_data = get_plugin_data( __FILE__, false );	// $markup = false
 					deactivate_plugins( $plugin, true );	// $silent = true
 					wp_die( 
-						'<p>'.sprintf( __( '%1$s requires WordPress version %2$s or higher and has been deactivated.',
-							'jsm-user-locale' ), $plugin_data['Name'], self::$wp_min_version ).'</p>'.
-						'<p>'.sprintf( __( 'Please upgrade WordPress before trying to reactivate the %1$s plugin.',
-							'jsm-user-locale' ), $plugin_data['Name'] ).'</p>'
+						'<p>'.sprintf( __( '%1$s requires %2$s version %3$s or higher and has been deactivated.',
+							'jsm-user-locale' ), $plugin_data['Name'], 'WordPress', self::$wp_min_version ).'</p>'.
+						'<p>'.sprintf( __( 'Please upgrade %1$s before trying to reactivate the %2$s plugin.',
+							'jsm-user-locale' ), 'WordPress', $plugin_data['Name'] ).'</p>'
 					);
 				}
 			}
