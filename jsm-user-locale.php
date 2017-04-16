294d253cd5bd7b83dc78b37dd16e3b0f113291bd
JS Morisset <jsm@surniaulula.com>
2017-04-16 09:08:10 +0200

    v1.2.2

--- jsm-user-locale.php
+++ jsm-user-locale.php
@@ -32,7 +32,6 @@ if ( ! class_exists( 'JSM_User_Locale' ) ) {
 	class JSM_User_Locale {
 
 		private static $instance;
-		private static $wp_min_version = 4.7;
 
 		private static $dashicons = array(
 			100 => 'admin-appearance',
@@ -233,7 +232,9 @@ if ( ! class_exists( 'JSM_User_Locale' ) ) {
 
 		public static function check_wp_version() {
 			global $wp_version;
-			if ( version_compare( $wp_version, self::$wp_min_version, '<' ) ) {
+			$wp_min_version = 4.7;
+
+			if ( version_compare( $wp_version, $wp_min_version, '<' ) ) {
 				$plugin = plugin_basename( __FILE__ );
 				if ( is_plugin_active( $plugin ) ) {
 					self::load_textdomain();
@@ -244,7 +245,7 @@ if ( ! class_exists( 'JSM_User_Locale' ) ) {
 					deactivate_plugins( $plugin, true );	// $silent = true
 					wp_die( 
 						'<p>'.sprintf( __( '%1$s requires %2$s version %3$s or higher and has been deactivated.',
-							'jsm-user-locale' ), $plugin_data['Name'], 'WordPress', self::$wp_min_version ).'</p>'.
+							'jsm-user-locale' ), $plugin_data['Name'], 'WordPress', $wp_min_version ).'</p>'.
 						'<p>'.sprintf( __( 'Please upgrade %1$s before trying to re-activate the %2$s plugin.',
 							'jsm-user-locale' ), 'WordPress', $plugin_data['Name'] ).'</p>'
 					);
