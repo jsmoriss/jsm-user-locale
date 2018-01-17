f4d58c7b448884b2b1a6c7ab5b60693148d94f69
JS Morisset <jsm@surniaulula.com>
2018-01-17 07:46:21 -0600

    v1.2.2

--- jsm-user-locale.php
+++ jsm-user-locale.php
@@ -12,7 +12,7 @@
  * Description: Add a quick and easy user locale / language selector in the WordPress admin back-end and front-end toolbar menus. 
  * Requires PHP: 5.4
  * Requires At Least: 4.7
- * Tested Up To: 4.9.1
+ * Tested Up To: 4.9.2
  * Version: 1.2.2
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
