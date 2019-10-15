435cda53a1300d055601d7803d04d2bdd8c64ee7
JS Morisset <jsm@surniaulula.com>
2019-10-15 10:11:42 -0700

    v1.2.2

--- jsm-user-locale.php
+++ jsm-user-locale.php
@@ -378,7 +378,7 @@ if ( ! class_exists( 'JSM_User_Locale' ) ) {
 			$menu_title = apply_filters( 'jsm_user_locale_menu_title', '%s', $menu_locale );
 			$menu_title = sprintf( $menu_title, $menu_locale );
 
-			$wp_admin_bar->add_node( array(	// Since WP v3.1.
+			$wp_admin_bar->add_node( array(
 				'id'     => 'jsm-user-locale',
 				'title'  => $menu_icon . $menu_title,
 				'parent' => false,
