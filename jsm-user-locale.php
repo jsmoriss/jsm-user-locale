4f1f5cd0896b0bbf4445763687c6e348ee7b0753
JS Morisset <jsm@surniaulula.com>
2023-04-13 06:44:38 -0700

    v2.2.0

--- jsm-user-locale.php
+++ jsm-user-locale.php
@@ -11,7 +11,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Add a quick and easy user locale / language selector in the WordPress admin back-end and front-end toolbar menus.
  * Requires PHP: 7.2
- * Requires At Least: 5.4
+ * Requires At Least: 5.5
  * Tested Up To: 6.2.0
  * Version: 2.2.0
  *
